1e9fb19cb0677ff9870c03c390921ee6ef94abe4
unknown <ronaldhoang@umail.ucsb.edu>
2018-02-20 19:42:17 -0800

    why wont it work

--- app/src/main/java/example/jackliu/nonewfriends/sendMsg.java
+++ app/src/main/java/example/jackliu/nonewfriends/sendMsg.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package example.jackliu.nonewfriends;
 
 import android.content.Intent;
@@ -30,36 +29,3 @@ public class sendMsg extends AppCompatActivity {
         });
     }
 }
-=======
-package example.jackliu.nonewfriends;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Button;
-
-public class sendMsg extends AppCompatActivity {
-    private Button sendbutton;
-
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.message_page);
-        sendbutton = (Button) findViewById(R.id.sendButton) ;
-
-
-
-
-        sendbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                startActivity(new Intent(sendMsg.this, sentMsgScreen.class));
-                finish();
-            }
-        });
-    }
-}
->>>>>>> 8972a911f9d3618269ead85292c7b20197b556c2
