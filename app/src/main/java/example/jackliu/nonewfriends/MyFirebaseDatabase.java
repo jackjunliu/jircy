09013ff2eb7feb0122c7895fa0746d29fcb378f1
yoonsz25 <yoonsz25@yahoo.com>
2018-03-05 13:47:27 -0800

    current location is up and running. May need to log off and log in the first time. In-progress: change heatmap to markers

---
+++ app/src/main/java/example/jackliu/nonewfriends/MyFirebaseDatabase.java
@@ -0,0 +1,43 @@
+package example.jackliu.nonewfriends;
+
+import android.location.Location;
+import android.util.Log;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+/**
+ * Created by yoonlee on 3/5/18.
+ */
+
+public class MyFirebaseDatabase {
+    private static final String TAG = "MyFirebaseDatabase";
+
+    private FirebaseAuth auth;
+    private FirebaseDatabase database;
+    private LocationData loc;
+
+    private String user;
+
+    public MyFirebaseDatabase(){
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        user = auth.getCurrentUser().getEmail();
+        loc = new LocationData().sender(auth.getCurrentUser().getEmail());
+    }
+
+    public void addLocation(Location coordinates){
+        loc.lat(coordinates.getLatitude())
+                .lng(coordinates.getLongitude())
+                .timestamp(System.currentTimeMillis() / 1000L);
+        String child = auth.getCurrentUser().getEmail();
+        DatabaseReference myRef = database.getReference();
+        myRef.child("location").child(child.substring(0,child.length()-4))
+                .setValue(loc);
+    }
+}
