feef42abfe0c73c3428e45941e288ddc672bae43
Irene (Pawena) Pattarachanyakul <ppattarachanyakul@umail.ucsb.edu>
2018-03-12 20:05:14 -0700

    added lat and long for msging

--- app/src/main/java/example/jackliu/nonewfriends/MessageActivity.java
+++ app/src/main/java/example/jackliu/nonewfriends/MessageActivity.java
@@ -2,11 +2,13 @@ package example.jackliu.nonewfriends;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
@@ -29,6 +31,10 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 
+import android.location.LocationManager;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
 
 
 public class MessageActivity extends AppCompatActivity {
@@ -41,6 +47,11 @@ public class MessageActivity extends AppCompatActivity {
     private EditText SendRecipient, MessageBroadcast;
 
     private Button sendButton, sendMessageButton;
+
+    private double longitude, latitude;
+    private Location LastLocation;
+    public LocationManager loc_man;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,7 +76,7 @@ public class MessageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_message);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             // Create channel to show notifications.
-            String channelId  = getString(R.string.default_notification_channel_id);
+            String channelId = getString(R.string.default_notification_channel_id);
             String channelName = getString(R.string.default_notification_channel_name);
             NotificationManager notificationManager =
                     getSystemService(NotificationManager.class);
@@ -137,7 +148,52 @@ public class MessageActivity extends AppCompatActivity {
                 }
             }
         });
+
+        int LOCATION_REFRESH_TIME = 1000; // 1000 ms
+        int LOCATION_REFRESH_DISTANCE = 2000; //2 km
+
+        loc_man = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        loc_man.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME, LOCATION_REFRESH_DISTANCE, loc_listener);
+
+
     }
+
+
+
+    private final LocationListener loc_listener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            LastLocation = location;
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            System.out.println("onStatusChanged");
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            System.out.println("onProviderEnabled");
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            System.out.println("onProviderDisabled");
+            //turns off gps services
+        }
+    };
 //    private void sendNotification(final String msg) {
 //        AsyncTask.execute(new Runnable() {
 //            @Override
@@ -212,6 +268,8 @@ public class MessageActivity extends AppCompatActivity {
 //            }
 //        });
 //    }
+
+
     public void emailCheck(final String msg) {
 
         auth.fetchProvidersForEmail(msg)
@@ -274,10 +332,11 @@ public class MessageActivity extends AppCompatActivity {
                         con.setRequestMethod("POST");
 
                         String strJsonBody = "{"
+                                //+ "\"filters\": [{\"field\": \"tag\", \"key\": \"User_ID\", \"relation\": \"=\", \"value\": \"" + send_email + "\"}],"
                                 //needs unique app_id from OneSignal - this is Yoon's
                                 + "\"app_id\": \"c9cf3c94-40f2-4f67-aeeb-e83db45aa5f6\","
 
-                                + "\"filters\": [{\"field\": \"tag\", \"key\": \"User_ID\", \"relation\": \"=\", \"value\": \"" + send_email + "\"}],"
+                                + "\"filters\": [{\"field\": \"location\", \"radius\": \"2000\", \"lat\": \" " + latitude + "\", \"long\": \"" + longitude + "\"],"
 
                                 + "\"data\": {\"foo\": \"bar\"},"
                                 + "\"contents\": {\"en\": \"" + message + "\"}"
